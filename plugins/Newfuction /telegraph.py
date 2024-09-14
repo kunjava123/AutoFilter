b73eaae60229b2c559cb0928d0f94a3d94fb9f6b
mn-bots <mohammedmusammiln@gmail.com>
2024-09-14 15:20:13 +0530

    Update telegraph.py

--- plugins/Newfuction /telegraph.py
+++ plugins/Newfuction /telegraph.py
@@ -1,41 +1,35 @@
+import os
+import requests
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from Mangandi import ImageUploader, VideoUploader
+from pyrogram.types import Message
 
-@Client.on_message(filters.command(["mangandi", "telegraph", "img2link", "manga", "ma","vid2link"]))
-async def mangandi(client, message):
-    if message.reply_to_message:
-        if message.reply_to_message.photo:
-            photo = message.reply_to_message.photo
-            if photo.file_size > 500 * 1024 * 1024:  # 500 MB
-                await message.reply("Image size is too large! Maximum allowed size is 500 MB")
-                return
-            k = await message.reply_to_message.download()
-            up = ImageUploader(k)
-            link = up.upload()
-            keyboard = InlineKeyboardMarkup([
-                [InlineKeyboardButton("Share Link", url=f"https://telegram.me/share/url?url={link}"),
-                 InlineKeyboardButton("Copy Link", url=f"{link}")]
-            ])
-            k = await message.reply_text("`Downloading To My Server ...`")
-            await k.edit("`Downloading Completed. Now I am Uploading ...`")
-            await k.edit(f"**Link: {link}**\n\n**Copy link:** `{link}`\n\n>**By: @mnbots**", reply_markup=keyboard)
-        elif message.reply_to_message.video:
-            video = message.reply_to_message.video
-            if video.file_size > 500 * 1024 * 1024:  # 500 MB
-                await message.reply("Video size is too large! Maximum allowed size is 500 MB")
-                return
-            k = await message.reply_to_message.download()
-            up = VideoUploader(k)
-            link = up.upload()
-            keyboard = InlineKeyboardMarkup([
-                [InlineKeyboardButton("Share Link", url=f"https://telegram.me/share/url?url={link}"),
-                 InlineKeyboardButton("Copy Link", url=f"{link}")]
-            ])
-            k = await message.reply_text("`Downloading To My Server ...`")
-            await k.edit("`Downloading Completed. Now I am Uploading ...`")
-            await k.edit(f"**Link: {link}**\n\n**Copy link:** `{link}`\n\n>**By: @mnbots**", reply_markup=keyboard)
-        else:
-            await message.reply("Please reply to a photo or video")
-    else:
-        await message.reply("Please reply to a photo or video")
+@Client.on_message(filters.command(["imglink", "cup", "telegraph"], prefixes="/") & filters.reply)
+async def c_upload(client, message: Message):
+    reply = message.reply_to_message
+
+    if not reply.media:
+        return await message.reply_text("Reply to a media to upload it to Cloud.")
+
+    if reply.document and reply.document.file_size > 512 * 1024 * 1024:  # 512 MB
+        return await message.reply_text("File size limit is 512 MB.")
+
+    msg = await message.reply_text("Processing...")
+
+    try:
+        downloaded_media = await reply.download()
+
+        if not downloaded_media:
+            return await msg.edit_text("Something went wrong during download.")
+
+        with open(downloaded_media, "rb") as f:
+            data = f.read()
+            resp = requests.post("https://envs.sh", files={"file": data})
+            if resp.status_code == 200:
+                await msg.edit_text(f"{resp.text}")
+            else:
+                await msg.edit_text("Something went wrong. Please try again later.")
+
+        os.remove(downloaded_media)
+
+    except Exception as e:
+        await msg.edit_text(f"Error: {str(e)}")
