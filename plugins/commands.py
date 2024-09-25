f7189210e5fc45f3f5710a871259f63727e6dbe0
𝐕𝐈𝐏𝐄𝐑𝐗 <sreeragplaystation@gmail.com>
2024-09-25 18:34:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
-                InlineKeyboardButton('🤖 Updates', url=(@mk_movies_linkz))
+                InlineKeyboardButton('🤖 Updates', url=("https://t.me/mk_movies_linkz"))
             ],
             [
                 InlineKeyboardButton('ℹ️ ʜᴇʟᴘ', url=f"https://t.me/{temp.U_NAME}?start=help"),
