9a9ee92ff8655d0d7009dac8b1ff95e96c457150
Madox <madox000000@gmail.com>
2024-09-27 13:39:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -49,8 +49,8 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('⚡Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⚡', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('🍷 Bᴏᴛ Oᴡɴᴇʀ', callback_data=("https://t.me/stroker_kidd")
-                    InlineKeyboardButton('🌈Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=("https://t.me/movies_kottaaram")
+                    InlineKeyboardButton('🍷 Bᴏᴛ Oᴡɴᴇʀ', callback_data='about')
+                    InlineKeyboardButton('🌈Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url="https://t.me/movies_kottaaram")
                 ],[
                     InlineKeyboardButton('🌀 Hᴇʟᴘ🌀', callback_data='help'),
                     InlineKeyboardButton(' 🌀Aʙᴏᴜᴛ🌀', callback_data='about')
@@ -79,7 +79,7 @@ async def start(client, message):
             else:
  https"          channel_num = str(chnl_num)+"ᴛʜ"
             btn.append([
-                InlineKeyboardButton(f"❆ Jᴏɪɴ {channel_num} Cʜᴀɴɴᴇʟ ❆",) ("url=https://t.me/mk_movies_linkz")
+                InlineKeyboardButton(f"❆ Jᴏɪɴ {channel_num} Cʜᴀɴɴᴇʟ ❆", "url=https://t.me/mk_movies_linkz")
             ])
 
         if message.command[1] != "subscribe":
@@ -100,7 +100,7 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('⚡Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⚡', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('🍷 Bᴏᴛ Oᴡɴᴇʀ', callback_data=https://t.me/stroker_kidd")
+                    InlineKeyboardButton('🍷 Bᴏᴛ Oᴡɴᴇʀ', callback_data='about')
                     InlineKeyboardButton("'🌈Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=https://t.me/movies_kottaaram")
                 ],[
                     InlineKeyboardButton('🌀 Hᴇʟᴘ🌀', callback_data='help'),
@@ -179,7 +179,7 @@ async def start(client, message):
                           InlineKeyboardButton('Movie Group', url="https://t.me/movies_kottaaram")
                           InlineKeyboardButton('Movie Updates', url="https://t.me/mk_movies_linkz")
                        ],[
-                          InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", ("url=https://t.me/stroker_kidd")
+                          InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", "url=https://t.me/stroker_kidd")
                          ]
                         ]
                     )
@@ -295,10 +295,10 @@ async def start(client, message):
                 reply_markup=InlineKeyboardMarkup(
                     [
                      [
-                      InlineKeyboardButton('Movie Group',) ("url=https://t.me/movies_kottaaram")
-                      InlineKeyboardButton('Movie Updates',) ("url=https://t.me/mk_movies_linkz")
+                      InlineKeyboardButton('Movie Group',("url=https://t.me/movies_kottaaram")
+                      InlineKeyboardButton('Movie Updates', "url=https://t.me/mk_movies_linkz")
                    ],[
-                      InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", ("url=https://t.me/stroker_kidd")
+                      InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", "url=https://t.me/stroker_kidd")
                      ]
                     ]
                 )
@@ -349,10 +349,10 @@ async def start(client, message):
         reply_markup=InlineKeyboardMarkup(
                    [
                      [
-                      InlineKeyboardButton('Movie Group',) ("url=https://t.me/mk_movies_linkz")
-                      InlineKeyboardButton('Movie Updates',) ("url=https://t.me/mk_movies_linkz")
+                      InlineKeyboardButton('Movie Group', "url=https://t.me/mk_movies_linkz")
+                      InlineKeyboardButton('Movie Updates',"url=https://t.me/mk_movies_linkz")
                    ],[
-                      InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ",) ("url=https://t.me/stroker_kidd")
+                      InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", "url=https://t.me/stroker_kidd")
                      ]
                     ]
         )
